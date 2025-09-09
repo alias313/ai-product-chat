87ad19f948f9cc840d410c2d3ec1c6c098942814
Elies <68759092+alias313@users.noreply.github.com>
2025-09-10 00:07:08 +0200

    Not leaking the API keys for once

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/generate-product", {
+      method: "POST",
+      body: JSON.stringify({
+        userMessage: "I want to design a modular car",
+        conversationHistory: [],
+        isStart: true,
+      }),
+    }).then(res => res.json())
+      .then(data => {
+        console.log("Generated product", data);
+      })
+      .catch(error => {
+        console.error("Error generating product", error);
+      });
+  })
   return (
     <div className="font-sans min-h-screen p-8">
       Product design chat app
