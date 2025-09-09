6d3aa7d5d3c24cbb42352d324a85e931bb03ba39
Elies <68759092+alias313@users.noreply.github.com>
2025-09-09 23:43:02 +0200

    Consts done

---
+++ src/lib/consts.ts
@@ -0,0 +1,21 @@
+export const UI_MESSAGES = {
+    LOADING: {
+        PROMPT: "Generating promt...", 
+        IMAGE: "Generating image...",
+    },
+    ERROR: {
+        PROMPT_GENERATION: "Error generating prompt",
+        IMAGE_GENERATION: "Error generating image",
+        MISSING_PROMPT: "Missing prompt",
+    },
+    PLACEHOLDERS: {
+        INPUT: "Describe what you want to design",
+    }
+}
+
+export const CHAT_CONFIG = {
+    IMAGE: {
+        DEFAULT_PROMPT: `A futuristic modular car, as a product designer creating a render of it with studio lighting following japanese kintsugi design philosophy as the shell of the car, it should be slightly visible through translucent material.`,
+        SEPARATOR: "IMAGE:",
+    }
+}
